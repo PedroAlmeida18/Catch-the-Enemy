949bfb60b8c682d8a6e5cc48db7f8456be236162
Pedro Almeida <pedrohas35@gmail.com>
2024-05-15 14:32:36 -0300

    Fase1 com 5 inimigos

--- Assets/Script/NovosInimigos.cs
+++ Assets/Script/NovosInimigos.cs
@@ -12,6 +12,7 @@ public class NovosInimigos : MonoBehaviour
 {
     public Controla_Inimigo Inimigos ;
     private float TempoEmcriacao;
+     private int quantidadeInimigos;
     void Start()
     {
         TempoEmcriacao = 0;
@@ -20,7 +21,11 @@ public class NovosInimigos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
       criacoInimigos();
+            
+            
+      //criacoInimigos();
     }
     private void criacoInimigos(){
         TempoEmcriacao+=Time.deltaTime;
@@ -29,7 +34,16 @@ public class NovosInimigos : MonoBehaviour
              float eixox = Random.Range(-9,9);
             float eixoy = Random.Range(-4,4);
             UnityEngine.Vector2 psocaoInimigo = new  UnityEngine.Vector2(eixox,eixoy);
-            Instantiate(Inimigos, psocaoInimigo, Quaternion.identity);
+            while (quantidadeInimigos<5)
+            {
+                Instantiate(Inimigos, psocaoInimigo, Quaternion.identity);
+                quantidadeInimigos++;
+            }
+                quantidadeInimigos=5;
+                Debug.Log("Unatidade de Inimgos criados foi :"+ quantidadeInimigos);
+            
+            
+            
             
         }
         
